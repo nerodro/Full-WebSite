c82232bbcfc17d161dea230503601c545bfe7bd2
nerodro <45393799+nerodro@users.noreply.github.com>
2021-06-01 10:41:09 +0300

    Update SendForm.cs

--- Test_Resiter/Test_Resiter/Models/SendForm.cs
+++ Test_Resiter/Test_Resiter/Models/SendForm.cs
@@ -9,7 +9,6 @@ namespace Test_Resiter.Models
     public class SendForm
     {
         public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
